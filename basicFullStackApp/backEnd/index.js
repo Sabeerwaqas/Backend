b66e0915c5945593aa5bc43098c2fd30e8c9ab36
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-03-02 01:00:06 +0500

    [Refactor: Clone freecodecamp git repo and complete npm section]

--- basicFullStackApp/backEnd/index.js
+++ basicFullStackApp/backEnd/index.js
@@ -1,38 +1,36 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-const PORT = process.env.PORT || 3050;
+app.use(cors());
 
-// app.get("/", (req, res)=>{
-//     res.send("Server is ready");
-// })
+const PORT = 5000;
 
 // get a list of dummy data
 
-app.get('/dummyData', (req, res)=>{
-    const data = [
-        {
-            id:1,
-            name:"John"
-        },
-        {
-            id:2,
-            name:"David"
-        },
-        {
-            id:3,
-            name:"Stephen"
-        },
-        {
-            id:4,
-            name:"Justine"
-        }
-    ]
-    res.send(data);
-})
+app.get("/api/dummyData", (req, res) => {
+  const data = [
+    {
+      id: 1,
+      name: "John",
+    },
+    {
+      id: 2,
+      name: "David",
+    },
+    {
+      id: 3,
+      name: "Stephen",
+    },
+    {
+      id: 4,
+      name: "Justine",
+    },
+  ];
+  res.send(data);
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Serving at http://localhost:${PORT}`);
-})
-
+app.listen(PORT, () => {
+  console.log(`Serving at http://localhost:${PORT}`);
+});
--- basicFullStackApp/frontEnd/src/App.jsx
+++ basicFullStackApp/frontEnd/src/App.jsx
@@ -1,17 +1,14 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import axios from "axios";
-
-
-
 
 function App() {
   const [data, setData] = useState([]);
 
   const getData = () => {
-    axios.get("http://localhost:3050/dummyData")
+    axios.get("http://localhost:5000/api/dummyData")
     .then((response)=>{
       console.log("Data=>", response.data)
       setData(response.data)
@@ -35,7 +32,7 @@ function App() {
     // You need to return JSX from the map function
     return (
       <h1 key={index}>
-        {person} {/* Assuming person is a string */}
+        {person.name} {/* Assuming person is a string */}
       </h1>
     );
   })
